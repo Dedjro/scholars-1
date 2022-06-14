99a8b839526549a8e8b8019dc3bdd7162d2ea712
fortunedavis <fortune.kponou@gmail.com>
2022-06-14 15:09:42 +0100

    make it better

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,51 @@
+class Admin::PostsController < ApplicationController
+  layout 'admin'
+  before_action :set_post, only: [:destroy,:update,:show,:edit]
+  def index
+      @posts = Post.all
+      # @items = params[:items] || 25
+      # @pagy, @posts = pagy(@posts, items: @items)
+      # @posts = @posts.decorate
+  end
+  
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to admin_posts_path, notice: "Enregistrement effectué avec succès"
+    end  
+
+  end
+
+  def edit
+  end
+  
+  def update
+    if @post.update(post_params)
+      redirect_to admin_posts_path, notice: "Modification effectuée avec succès"
+    end
+  end
+
+  def show
+    
+  end
+
+  def destroy
+    if @post.destroy
+      redirect_to admin_posts_path, notice: "Suppression effectuée avec succès"
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.friendly.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:content, :title)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  #devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+ 
+  namespace :admin do
+    get '/', :to => 'dashboard#index'
+
+    resources :dashboard, only: [:index]
+    resources :posts
+
+  end
+ # resources :posts
+  root "home#index"
+end
